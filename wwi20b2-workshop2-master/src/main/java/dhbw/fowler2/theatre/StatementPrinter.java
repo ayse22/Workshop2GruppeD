74fc64a612c1ddb08efdfd9d232272fe8844b8c8
MarcoPelzer <marco.pelzer99@web.de>
2022-04-09 11:50:47 +0200

    Extract Method

--- wwi20b2-workshop2-master/src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ wwi20b2-workshop2-master/src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -15,38 +15,45 @@ public class StatementPrinter {
 
         for (var perf : invoice.performances) {
             var play = plays.get(perf.playID);
-            var thisAmount = 0;
-
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
-                    }
-                    thisAmount += 300 * perf.audience;
-                    break;
-                default:
-                    throw new Error("unknown type: ${play.type}");
-            }
-
-            // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-            // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
-            totalAmount += thisAmount;
+            var thisAmount = amountFor(perf, play);
         }
+
+        // add volume credits
+         volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+        
+         // print line for this order
+        result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+        totalAmount += thisAmount;
+                
         result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
     }
+        function amountFor(aPerformance play) {
+            let result = 0;
+            switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+            }
+            return result;
+        }
 
-}
+
+    }
+
+
